72e062189a63e027d8ae24ef3b4148dff86b0f2d
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-14 02:25:41 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,24 +7,25 @@ class Author
     @posts = []
   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@total_posts += 1
-  end
-
   def posts
     @posts
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
+  def add_post(post_name)
+    @posts << post_name
+    post_name.author = self
+    @@total_posts += 1
+  end
+
+  def add_post_by_title(post_title)
+    new_post = Post.new(post_title)
+    @posts << new_post
+    new_post.author = self
     @@total_posts += 1
   end
 
   def self.post_count
     @@total_posts
   end
+
 end
